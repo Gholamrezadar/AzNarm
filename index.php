c270c06255835c32dd1b51056c3f00835d4f9503
Gholamreza Dar <rezadar1378@gmail.com>
2019-12-29 00:33:06 +0330

    WordPress v.2

--- index.php
+++ index.php
@@ -17,6 +17,7 @@
     <div class="container">
 
       <div class="row">
+      <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
         <div class="col-md-4">
           <div class="card mb-4 shadow-sm">
             <div class="bd-placeholder-img card-img-top" width="100%" height="225">
@@ -24,11 +25,14 @@
             </div>
             
             <div class="card-body">
-            <h3><a href="#" class="post-title">چیزبرگر افریقایی</a></h3>
+            <h3><a href="<?php the_permalink(); ?>" class="post-title"><?php the_title(); ?></a></h3>
               <p class="card-text"> متن چرت و پرت متن چرت و پرت متن چرت و پرت متن چرت و پرت متن چرت و پرت متن چرت و پرت متن چرت و پرت متن چرت و پرت متن چرت و پرت</p>
             </div>
           </div>
         </div>
+        <?php endwhile; else: ?>
+                        غذایی برای نمایش وجود ندارد        
+      <?php endif; ?>
     </div>
   </div>
 
